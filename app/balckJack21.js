048809ccf943ce173931d7976bac6108359d5219
zicolino <167160363+zicolino@users.noreply.github.com>
2024-05-14 18:47:15 -0300

    Fix a la interfaz de usuario para no repartir manos de mas. Fix representacion de la mano del croupier

--- app/balckJack21.js
+++ app/balckJack21.js
@@ -34,20 +34,27 @@ function dealCards(numPlayers) {
 createDeck()
 shuffleDeck()
 
-let numPlayers = parseInt(prompt("Bienvenido/s a BlackJack21! \nCuantos jugadores enfrentaran al croupier?")+1)
+let numPlayers = 1 + parseInt(prompt("Bienvenido/s a BlackJack21! \nCuantos jugadores enfrentaran al croupier?"))
 
 dealCards(numPlayers)
 console.log("Croupier's Hand: ", playerHand[0])
 console.log("Player 1's Hand:", playerHand[1])
 
 let msg = "";
-for (let i = 0; i < numPlayers; i++) {
-    msg += `Player ${i + 1}'s hand` ;
+for (let i=0; i<1; i++) {
+    msg += `Croupier's hand: \n` ;
+    for (let card of playerHand[0]) {
+        msg += `[ ${card.rank} of ${card.suit} ] `
+    msg = msg.slice(0, -2);
+    msg += `\n` };
+for (let i = 1; i < numPlayers; i++) {
+    msg += `Player ${i}'s hand: \n` ;
     for (let card of playerHand[i]) {
-        msg += `[${card.rank} of ${card.suit}],` ;  
-    }
+        msg += `[ ${card.rank} of ${card.suit} ] `
     msg = msg.slice(0, -2);
     msg += `\n`
+}
+}
 }
 
 alert(msg)

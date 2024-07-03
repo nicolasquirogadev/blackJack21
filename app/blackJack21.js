82470b407d9533c10c4c92bcf9f06ddaa3853ab5
zicolino <167160363+zicolino@users.noreply.github.com>
2024-07-03 20:10:12 -0300

    Sumando comentarios al archivo .js para explicar el funcionamiento de la app

--- app/blackJack21.js
+++ app/blackJack21.js
@@ -1,4 +1,4 @@
-
+// Declarando variables generales de la app.
 var croupierSum = 0;
 var playerSum = 0;
 
@@ -10,12 +10,12 @@ let cardsDeck = []
 
 var canHit = true; 
 var roundWin;
-
+// Declarando los palos y valores de las cartas con las que vamos a crear el mazo.
 const suits = ['D', 'H', 'S', 'C']
 
 const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
-
+// Funcion para crear un mazo de cartas utilizando los arrays declarados anteriormente.
 function createDeck() {
     for (let suit of suits){
         for (let rank of ranks) {
@@ -23,26 +23,25 @@ function createDeck() {
         }
     }
 };
-
+// Funcion para mezclar el mazo de manera aleatoria, para evitar que toquen siempre las mismas cartas.
 function shuffleDeck(){
     for (let i = cardsDeck.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() *i);
     [cardsDeck[i], cardsDeck[j]] = [cardsDeck[j], cardsDeck[i]]
     }
 };
-
+// Anexamos el evento al boton START y declaramos la funcion principal para el funcionamiento del juego.
 document.getElementById("startbutton").addEventListener("click", startGame);
-
 function startGame() {
     createDeck();
     shuffleDeck();
     console.log(cardsDeck);
     
     canHit = true;
-
+// Llamamos a la funcion getChips que va a traer el valor del banco de fichas del jugador guardado en el localStorage del navegador, y lo imprimimos en pantalla.
     getChips();
     document.getElementById("chips-counter").innerText = getChips();
-
+// El croupier saca la primer carta del mazo pero ésta se muestra boca abajo en la mesa (oculta) y no se revela hasta el final de la ronda.
     let hiddenImg = document.createElement("img");
     hiddenImg.src = "./cards/BACK.png";
     hiddenImg.id = "hidden"
@@ -51,7 +50,7 @@ function startGame() {
     hidden = cardsDeck.pop();
     croupierSum += getValue(hidden);
     croupierAceCount += checkAce(hidden);
-
+// Luego de sacar la primer carta (oculta), el croupier sigue sacando cartas hasta llegar a un valor >17.
     while (croupierSum < 17) {
         let cardImg = document.createElement("img");
         let card = cardsDeck.pop();
@@ -61,7 +60,7 @@ function startGame() {
         document.getElementById("croupier-cards").append(cardImg);
     }
     console.log(`Croupier has: ` + croupierSum);
-
+// Se reparten dos cartas aleatorias al jugador.
     for (let i = 0; i < 2; i++) {
         let cardImg = document.createElement("img");
         let card = cardsDeck.pop();
@@ -71,13 +70,14 @@ function startGame() {
         document.getElementById("player-cards").append(cardImg);
     }
     console.log(`You have: ` + playerSum);
+    //Anexamos eventos para los botones de la interfaz e imprimimos el puntaje del jugador en pantalla, el del croupier permanecerá oculto hasta el final de la ronda.
     document.getElementById("ask-for-card").addEventListener("click", askForCard); 
     document.getElementById("stay").addEventListener("click", stay);
     document.getElementById("player-sum").innerText = playerSum;
 
 }
 
-
+// Funcion que marca las condiciones bajo las cuales el jugador puede o no pedir mas cartas.
 function askForCard() {
     if (!canHit) {
         return;
@@ -96,7 +96,7 @@ function askForCard() {
     document.getElementById("player-sum").innerText = playerSum;
 }
     
-    
+ // Funcion que marca el final de la ronda y notifica el resultado utilizando SweetAlert.   
 function stay() {
     canHit = false;
     document.getElementById("hidden").src = "./cards/" + hidden + ".png";
@@ -145,16 +145,7 @@ function stay() {
             icon: "error"
           });
     }
-    else if (playerSum == 21) {
-        msg = "BLACKJACK! \n You Win!"
-        roundWin = true;
-        Swal.fire({
-            title: "You win!",
-            text: `You have ${playerSum}! That's a BLACKJACK! `,
-            icon: "success"
-          });
-    }
-
+// Al ejecutar roundOutcome(), el valor de las fichas (chips) se actualiza segun el resultado de la ronda.
    roundOutcome();
 
     cardsDeck = [];
@@ -165,7 +156,7 @@ function stay() {
     document.getElementById("results").innerText = msg;
     document.getElementById("startbutton").addEventListener("click", resetGame);
 }
-
+// Funcion utilizada para limpiar la mesa al final de cada ronda.
 function resetGame() {
     
     const element1 = document.getElementById("player-cards");
@@ -194,28 +185,28 @@ function resetGame() {
 
     startGame();
 }
-
+// Funcion utilizada para obtener el valor de la carta cortando por el "-" y los guarda en un array que representa el valor de cada carta en mano.
 function getValue(card) {
-    let data = card.split("-");
+    let data = card.split("-"); // "7-H" -> ["7", "H"]
     let value = data[0];
 
-    if (isNaN(value)) {
+    if (isNaN(value)) { // A J Q K
         if (value == "A"){
             return 11;
         }
-        return 10;
+        return 10; // "K-D" -> ["10", "D"]
     }
-
+    //En caso de que la carta si sea un numero, parsea su valor.
     return parseInt(value);
 } 
-
+ 
 function checkAce(card) {
     if (card[0] == "A") {
         return 1;
     }
     return 0;
 }
-
+// Funcion que permite que el As sume 11 a los puntos del jugador, excepto que su suma exceda los 21 puntos, caso en el que el As convierte su valor a 1.
 function reduceAce (playerSum, playerAceCount) {
     while (playerSum > 21 && playerAceCount > 0) {
         playerSum -= 10;
